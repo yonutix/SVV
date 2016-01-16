0045f3ffb916b8ab32b8db76bc2f7a7d52812068
Andra <andraprav@gmail.com>
2016-01-16 11:12:29 +0200

    sections

--- restaurant/MainController.js
+++ restaurant/MainController.js
@@ -5,7 +5,23 @@
         var _selected;
 
           $scope.selected = undefined;
-          $scope.states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Dakota', 'North Carolina', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
+
+           $scope.go = function() {
+           $( "#accordion" ).hide();
+           alert($scope.search.date);
+            }
+         var restaurants = ['A', 'B', 'C', 'D', 'E', 'F'];
+         var kitchens=['k1','k2','k3','k4'];
+
+ $( "#input5" ).autocomplete({
+      source: restaurants
+    });
+ $( "#input6" ).autocomplete({
+      source: kitchens
+    });
+ $( "#accordion" ).accordion();
     };
     module.controller("MainController", MainController)
+
 }())
+
